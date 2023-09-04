88590b1af7a7688264b9f917e4e0d6df017bc32b
007kakashi <upendrasgosavi1@gmail.com>
2023-09-04 16:10:48 +0530

    notebook added

--- src/exception.py
+++ src/exception.py
@@ -19,4 +19,10 @@ class CustomException(Exception):
     
 
 
-    
+# if __name__=='__main__':
+#     try:
+#         a=1/0
+#     except Exception as e:
+#         logging.info('Divided by zero')
+#         raise CustomException(e,sys)
+
