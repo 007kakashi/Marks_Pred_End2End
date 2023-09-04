68a74d710b938d67c477cf21aa68f39ddc2829c9
007kakashi <upendrasgosavi1@gmail.com>
2023-09-04 13:38:32 +0530

    Exception.py added

--- src/logger.py
+++ src/logger.py
@@ -12,8 +12,7 @@ logging.basicConfig(
 
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO
+    level=logging.INFO,
 )
 
-if __name__ == '__main__':
-    logging.info('Logging has started')
+    
