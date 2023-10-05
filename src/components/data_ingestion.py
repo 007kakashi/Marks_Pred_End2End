6b92401e79380f38631ddd925cd5fef4a4fd69be
007kakashi <upendrasgosavi1@gmail.com>
2023-10-04 19:20:07 -0700

    'updated'

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -56,10 +56,10 @@ if __name__ == '__main__':
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_injegstion()
 
-    data_transformer=DataTransformation()
-    train_arr,test_arr,_=data_transformer.initiate_data_trasnformer(train_data,test_data)
+    # data_transformer=DataTransformation()
+    # train_arr,test_arr,_=data_transformer.initiate_data_trasnformer(train_data,test_data)
 
-    model_trainer=ModelTrainer()
-    print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+    # model_trainer=ModelTrainer()
+    # print(model_trainer.initiate_model_trainer(train_arr,test_arr))
 
 
